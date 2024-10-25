e69843bd9e7642a7426c1f31f061c44e4d5cf0fc
Mystery <koby.bradley@icloud.com>
2024-10-25 17:58:40 -0400

    Square Jump
    Configured the square to jump along with the proper jump physics. We froze it in place so it stays in its boundary and not bounce off and made the ground for it to stand on. And made and ground identify as ground.

--- Game Idea/Assets/Scripts/Jump.cs
+++ Game Idea/Assets/Scripts/Jump.cs
@@ -2,17 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Jump : MonoBehaviour
+public class PlayerJump : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float jumpForce = 500f; // Adjust this value to control jump height
+    public float fallMultiplier = 1f; // Controls fall speed
+
+    Rigidbody rb;
+    private bool isGrounded = true; // Tracks if player is grounded
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    private void Jump()
+    {
+        isGrounded = false;
+        rb.velocity = new Vector3(rb.velocity.x, 8, rb.velocity.z); // Reset y velocity before jumping
+        rb.AddForce(Vector3.up * jumpForce);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true; // Set isGrounded to true when hitting the ground
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
     }
 }
+
+
+
